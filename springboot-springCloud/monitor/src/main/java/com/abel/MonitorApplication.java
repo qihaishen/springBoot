8ddd0f10113bdfd5d8575ea23866a760b9aba4a3
qihaishen <qihaishenbj@163.com>
2019-11-15 16:33:48 +0800

    enable hystrix dashborad

--- springboot-springCloud/monitor/src/main/java/com/abel/MonitorApplication.java
+++ springboot-springCloud/monitor/src/main/java/com/abel/MonitorApplication.java
@@ -1,10 +1,14 @@
 package com.abel;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
+import org.springframework.context.annotation.Bean;
+
 /**
  * Created by yangyibo on 2018/6/29.
  */
@@ -16,5 +20,10 @@ public class MonitorApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MonitorApplication.class, args);
+    }
+
+    @Bean
+    public ServletRegistrationBean hystrixMetricsStreamServlet() {
+        return new ServletRegistrationBean(new HystrixMetricsStreamServlet(), "/hystrix.stream");
     }
 }
